93059e29e7120a5a91ca5a008e18a8784ba550b2
Aleksandr Batovkin <smailcmail@gmail.com>
2024-03-14 22:20:22 +0300

    fix issues
    issues: webpack генерирует index.html
    
    issues: фикс скелетона
    
    issues: фикс подпрыгивающей формы
    
    обновил фон
    
    issues: добавлена ссылка на деплой в README.md

--- src/components/Cards/FilmCard.js
+++ src/components/Cards/FilmCard.js
@@ -3,6 +3,10 @@ import { Component } from '../../core/src/Component.js';
 import { Rating } from '../Badges/Rating.js';
 
 class FilmCardInner extends Component {
+    state = {
+        loaded: false,
+    };
+
     fetchFilm = (ID) => {
         fetch(
             `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/content/contentPreview?${new URLSearchParams(
@@ -31,35 +35,37 @@ class FilmCardInner extends Component {
         return Core.createElement(
             'div',
             { class: 'card' },
-            Core.createElement(
-                'div',
-                { class: 'poster' },
-                rating,
-                Core.createElement('img', {
-                    src: `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/static/${state?.poster ?? ''}`,
-                    alt: 'Постер',
-                }),
-            ),
-            Core.createElement(
-                'div',
-                { class: 'card-info' },
-                Core.createElement('h5', state?.title ?? ''),
+            this.state.loaded &&
                 Core.createElement(
-                    'span',
-                    {},
-                    `${state?.original_title === '' ? state?.title ?? '' : state?.original_title}, ${state?.release_year ?? ''}, ${state?.duration ?? ''} мин.`,
+                    'div',
+                    { class: 'poster' },
+                    rating,
+                    Core.createElement('img', {
+                        src: `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/static/${state?.poster ?? ''}`,
+                        alt: 'Постер',
+                    }),
                 ),
+            this.state.loaded &&
                 Core.createElement(
-                    'span',
-                    {},
-                    `${state?.country ?? ''} ▸ ${state?.genre ?? ''} ▸ Режиссёр: ${state?.director ?? ''}`,
+                    'div',
+                    { class: 'card-info' },
+                    Core.createElement('h5', !!state?.title ?? ''),
+                    Core.createElement(
+                        'span',
+                        {},
+                        `${state?.original_title === '' ? state?.title ?? '' : state?.original_title}, ${state?.release_year ?? ''}, ${state?.duration ?? ''} мин.`,
+                    ),
+                    Core.createElement(
+                        'span',
+                        {},
+                        `${state?.country ?? ''} ▸ ${state?.genre ?? ''} ▸ Режиссёр: ${state?.director ?? ''}`,
+                    ),
+                    Core.createElement(
+                        'span',
+                        {},
+                        `В ролях: ${state?.actors ? state.actors.join(', ') : ''}`,
+                    ),
                 ),
-                Core.createElement(
-                    'span',
-                    {},
-                    `В ролях: ${state?.actors ? state.actors.join(', ') : ''}`,
-                ),
-            ),
         );
     }
 }
